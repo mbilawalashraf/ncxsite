9626b28993d679cdbd7fee363b10f57c8815c804
clydencx <clyde@ncx.cx>
2022-03-08 11:31:43 +0400

    navigating programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Home() {
+    const router = useRouter()
+
+    const handleClick = () => {
+        console.log('Placing your order')
+        router.push('/product')
+    }
+
     return (
         <>
             <Head>
@@ -17,6 +25,9 @@ function Home() {
                 <Link href='/product'>
                     <a>Products</a>    
                 </Link>
+                <button onClick={handleClick}>
+                    Place Order    
+                </button>
             </div>
         </>
     )
