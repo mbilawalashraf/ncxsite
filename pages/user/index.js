fa409ca74eb7ce3a428bf20cf30402a18c42f353
clydencx <clyde@ncx.cx>
2022-03-08 13:20:28 +0400

    pages vs components

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,3 +1,5 @@
+import User from "../../components/user"
+
 function UserList({ users }) {
     return (
         <>
@@ -6,8 +8,7 @@ function UserList({ users }) {
                 users.map(user => {
                     return (
                         <div key={user.id}>
-                            <p>{user.name}</p>
-                            <p>{user.email}</p>
+                            <User user={user} />
                         </div>
                     )
                 })
