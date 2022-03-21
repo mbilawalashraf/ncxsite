ce810783a0581cbe33cd2385452249094feab69b
clydencx <clyde@ncx.cx>
2022-03-21 17:01:01 +0400

    added few changes in buttons the p tag abd added title on the search area

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -66,7 +66,7 @@ function Landing({ coinsData }) {
                             <h1 className={styles.steps_title}>Get Started in Minutes</h1>
                         </div>
                     </div>
-                    <div className="grid grid-cols-4 my-14">
+                    <div className="grid grid-cols-4 mt-14">
                         <div>
                             <div className={styles.steps_background}>
                                 <a href="#" className="lg:inline">
@@ -128,9 +128,9 @@ function Landing({ coinsData }) {
                             </div>
                         </div>
                     </div>
-                    <div style={{textAlign: 'center'}}>
+                    <div className={styles.hero2_button}>
                     <a href="#"
-                        className="block px-12 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 lg:inline hover:bg-blue-400">Get Started
+                        className="">Get Started
                     </a>
                     </div>
                 </div>
@@ -143,6 +143,7 @@ function Landing({ coinsData }) {
             <div className="bg-white lg:h-full bg_coinslist">
                 <div className="max-w-screen-xl mx-auto">
                     <div className={styles.coin_scroll}>
+                        <h1>Select Crypto</h1>
                         <SearchBar type='text' placeholder='Search' onChange={handleChange} />
                         <TableHeaderLanding />
                         <CoinListLanding coinsData={filteredCoins} />
@@ -173,9 +174,9 @@ function Landing({ coinsData }) {
                                         </a>
                                     </div>
                                 </div>
-                                <div className="mt-6">
+                                <div className={styles.hero1_button}>
                                     <a href="#"
-                                        className="block px-12 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 lg:inline hover:bg-blue-400">Trade Now
+                                        className="">Trade Now
                                     </a>
                                 </div>
                             </div>
@@ -202,7 +203,7 @@ function Landing({ coinsData }) {
                         <div className="flex flex-col w-full lg:flex-row lg:w-1/2 ml-14 ">
                             <div className="max-w-lg">
                                 <h1 className="hero-title2">Fast Deposit &<br />Withdrawals</h1>
-                                <p className="mt-0 hero-content2">From Visa & Mastercard to instant SEPA<br />deposits, youve got options for getting the<br />funds you need, when you need them.</p>
+                                <p className={styles.hero_content_landing3}>From Visa & Mastercard to instant SEPA<br />deposits, youve got options for getting the<br />funds you need, when you need them.</p>
                                 <div className="grid grid-cols-4 store-logo2 py-4">
                                     <div className="">
                                         <a href="#"
@@ -212,9 +213,9 @@ function Landing({ coinsData }) {
                                         </a>
                                     </div>
                                 </div>
-                                <div style={{marginTop: 4.5 + 'rem'}}>
+                                <div className={styles.hero1_button}>
                                     <a href="#"
-                                        className="block px-12 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 lg:inline hover:bg-blue-400">Sign Up
+                                        className="">Sign Up
                                     </a>
                                 </div>
                             </div>
