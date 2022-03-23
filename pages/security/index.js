d3134aa869fb54ed8e6c1fa4ec5b072ad1e43ca0
clydencx <clyde@ncx.cx>
2022-03-23 14:26:36 +0400

    changed a few sizes on the first two sections in security page

--- pages/security/index.js
+++ pages/security/index.js
@@ -15,7 +15,7 @@ function Security() {
                         <div className="flex flex-col w-full lg:flex-row h1_parent_security">
                             <div className="max-w-lg">
                                 <h1 className={styles.hero1_title}>Global WEB 3.0<br />Cryptocurrency Exchange</h1>
-                                <p className="mt-0 hero-content">Buy, Sell  Trade over 100+ cryptocurrencies on NCX<br />Fast, Safe & Secure </p>
+                                <p className="mt-0 hero-content_security">Buy, Sell  Trade over 100+ cryptocurrencies on NCX<br />Fast, Safe & Secure </p>
                                 <div className="grid grid-cols-4 store-logo">
                                     <div className="">
                                         <a href="#"
@@ -131,6 +131,45 @@ function Security() {
                 </div>
             </div>
             {/* End Steps Section */}
+            {/* Hero Section 2 */}
+            <div className="bg-white lg:h-full lg:py-16">
+                <div className="max-w-screen-lg mx-auto">
+                <div className="container flex px-6 py-32 mx-auto">
+                        <div className="flex flex-col w-full lg:flex-row h1_parent">
+                            <div className="max-w-lg">
+                                <h1 className="hero-title">2FA Authentication</h1>
+                                <p className="mt-0 hero-content">Buy, Sell  Trade over 100+ cryptocurrencies on NCX<br />Fast, Safe & Secure </p>
+                                <div className="grid grid-cols-4 store-logo">
+                                    <div className="">
+                                        <a href="#"
+                                            className="lg:inline"><img className="appstore"
+                                            src="/appstore.svg" alt="Mock Up Data" />
+                                            
+                                        </a>
+                                    </div>
+                                    <div className="">
+                                        <a href="#"
+                                            className="lg:inline"><img className="googlestore"
+                                            src="/googleplay.svg" alt="Mock Up Data" />
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className={styles.hero1_button}>
+                                    <a href="#"
+                                        className="">Trade Now
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center justify-center w-full h-96 mock_parent">
+                            <img className="object-cover w-full h-full max-w-2xl rounded-md mockup-pic"
+                                src="/mockup.png" alt="Mock Up Data" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* End Hero Section 2 */}
             <Footer />
         </>
     )
