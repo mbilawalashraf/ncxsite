a3b1e6887a7bb2d90b0afdb6291c30c21549d010
clydencx <clyde@ncx.cx>
2022-03-24 13:40:43 +0400

    rephrased the main page

--- components/Nav.js
+++ components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
                         <div className="flex space-x-7">
                             <div>
                                 {/* <!-- Website Logo --> */}
-                                <a href="#" className="flex items-center py-6 px-2">
+                                <a href="#" className="flex items-center py-6 pr-2">
                                     <img src="/NCXlogo.svg" className="mr-3 h-6 sm:h-8" alt="NCX Logo" />
                                 </a>
                             </div>
@@ -15,32 +15,32 @@ function Nav() {
                             <div className="hidden md:flex items-center space-x-1 primary-nav">
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >Buy Crypto</a
                                 >
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >Exchange</a
                                 >
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >NCX Card</a
                                 >
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >About</a
                                 >
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >Blog/News</a
                                 >
                                 <a
                                     href=""
-                                    className="py-4 px-5"
+                                    className="py-4 pr-8"
                                     >Security</a
                                 >
                             </div>
@@ -48,12 +48,12 @@ function Nav() {
                         <div className="hidden md:flex items-center space-x-6 secondary-nav">
                                 <a
                                     href=""
-                                    className="py-2 px-5 border-2 border-black"
+                                    className="py-2 px-5 border-2 border-black sign_up"
                                     >Sign Up</a
                                 >
                                 <a
                                     href=""
-                                    className="py-2 px-5 font-medium text-white bg-blue-500 border-2 border-blue-500"
+                                    className="py-2 px-5 font-medium text-white bg-blue-500 border-1 border-blue-500 sign_in"
                                     >Sign In</a
                                 >
                             </div>
